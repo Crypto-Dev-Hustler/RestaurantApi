34f5e7c1c7db0452354fc3460151aa868cdb3a70
AYUSHMAN <ayushman311214@gmail.com>
2024-12-07 12:52:25 +0530

    add OrderService

--- src/main/java/com/bytebattler/Restaurant/Repository/OrderRepository.java
+++ src/main/java/com/bytebattler/Restaurant/Repository/OrderRepository.java
@@ -2,6 +2,8 @@ package com.bytebattler.Restaurant.Repository;
 
 import com.bytebattler.Restaurant.Models.OrderModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-interface OrderRepository extends JpaRepository<OrderModel, Long> {
+@Repository
+public interface OrderRepository extends JpaRepository<OrderModel, Long> {
 }
--- src/main/java/com/bytebattler/Restaurant/Services/OrderService.java
+++ src/main/java/com/bytebattler/Restaurant/Services/OrderService.java
@@ -1,4 +1,68 @@
 package com.bytebattler.Restaurant.Services;
 
+
+import com.bytebattler.Restaurant.Models.OrderModel;
+import com.bytebattler.Restaurant.Repository.OrderRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+	private static final Logger logger = LoggerFactory.getLogger(OrderService.class)
+
+	@Autowired
+	private OrderRepository orderRepository;
+
+	// Create
+	public OrderModel createOrder(OrderModel order) {
+		return orderRepository.save(order);
+	}
+
+	// Read
+	public List<OrderModel> getAllOrders() {
+		return orderRepository.findAll();
+	}
+
+	public Optional<OrderModel> getOrderById(Long id) {
+		return orderRepository.findById(id);
+	}
+
+	// Update
+	public boolean updateOrder(Long id, OrderModel orderDetails) {
+		Optional<OrderModel> existingOrderOptional = orderRepository.findById(id);
+		if (existingOrderOptional.isPresent()) {
+			OrderModel existingOrder = existingOrderOptional.get();
+			existingOrder.setStatus(orderDetails.getStatus());
+			existingOrder.setItems(orderDetails.getItems());
+			orderRepository.save(existingOrder);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// Delete
+	public boolean deleteOrder(Long id) {
+		if (orderRepository.existsById(id)) {
+			orderRepository.deleteById(id);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean deleteOrderById(Long id) {
+		if (orderRepository.existsById(id)) {
+			orderRepository.deleteById(id);
+			return true;
+		} else {
+			logger.error("Order with id {} not found", id);
+			return false;
+		}
+	}
+
 }
